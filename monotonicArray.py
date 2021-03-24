4ad374c619b31348eeddb08b47628c9be2f81e4b
chrisalopez1337 <chrisalopez@outlook.com>
2021-03-23 19:17:38 -0700

    Added working solution for isMonotonic

--- monotonicArray.py
+++ monotonicArray.py
@@ -10,4 +10,33 @@ def test(expected, actual):
         print('Expected:', expected, 'Actual:', actual)
 
 def isMonotonic(array):
+    direction = None
+    i = 0 
+    j = 1
+    while j < len(array):
+        if direction == None:
+            if array[j] == array[i]:
+                i += 1
+                j += 1
+                continue
+            elif array[i] < array[j]:
+                i += 1
+                j += 1
+                direction = 'Increase'
+            else:
+                i += 1
+                j += 1
+                direction = 'Decrease'
+        elif direction == 'Increase':
+            if array[i] > array[j]:
+                return False
+            i += 1
+            j += 1
+        elif direction == 'Decrease':
+            if array[i] < array[j]:
+                return False
+            i += 1
+            j += 1
+    return True
 
+test(expected, isMonotonic(array))
