f6c9777bf5f104deeb0290f56e5ee83edf6a43ce
chrisalopez1337 <chrisalopez@outlook.com>
2021-03-24 21:24:25 -0700

    Added setup for longestPeak problem

---
+++ longestPeak.py
@@ -0,0 +1,12 @@
+# Sample input
+array = [ 1, 2, 3, 3, 4, 0, 10, 6, 5, -1, -3, 2, 3]
+expected = 6
+
+# Simple test
+def test(exepcted, actual):
+    if expected == actual:
+        print('Working')
+    else:
+        print('Expected:', expected, 'Actual:', actual)
+
+
