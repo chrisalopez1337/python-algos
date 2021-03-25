2acf7d9b3194c9edefb7aacbf6e781e286a03d7b
chrisalopez1337 <chrisalopez@outlook.com>
2021-03-24 21:39:14 -0700

    Added working solution for longest peak problem

--- longestPeak.py
+++ longestPeak.py
@@ -9,4 +9,26 @@ def test(exepcted, actual):
     else:
         print('Expected:', expected, 'Actual:', actual)
 
+def longestPeak(array):
+	longestPeak = 0
+	i = 1
+	while i < len(array) - 1:
+		isPeak = array[i-1] < array[i] and array[i+1] < array[i]
+		if not isPeak:
+			i += 1
+			continue
+		# Accumulate left side
+		left = i - 2
+		while left >= 0 and array[left] < array[left+1]:
+			left -= 1
+		# Accumulate right side
+		right = i + 2
+		while right < len(array) and array[right] < array[right - 1]:
+			right += 1
+		
+		total = right - left - 1
+		longestPeak = max(longestPeak, total)
+		i = right
+	return longestPeak
 
+test(expected, longestPeak(array))
