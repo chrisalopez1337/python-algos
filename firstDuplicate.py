d861b30be6de66e23e43b283390985d942ff43d6
chrisalopez1337 <chrisalopez@outlook.com>
2021-03-24 22:17:51 -0700

    Added simple solution for firstDuplicate

--- firstDuplicate.py
+++ firstDuplicate.py
@@ -2,7 +2,14 @@ from test import test
 array = [2, 1, 5, 2, 3, 3, 4]
 expected = 2
 
+# Super simple naive approach
 def firstDuplicateValue(array):
-
+    Found = {}
+    for x in array:
+        if x in Found:
+            return x
+        else:
+            Found[x] = x
+    return -1
 
 test(expected, firstDuplicateValue(array))
